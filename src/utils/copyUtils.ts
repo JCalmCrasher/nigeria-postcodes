8e61aea32f5bacb2cc47473145c245155bc805f5
JoshX <joshx.dev@gmail.com>
2025-01-02 13:05:14 +0100

    fix typings

--- src/utils/copyUtils.ts
+++ src/utils/copyUtils.ts
@@ -1,12 +1,21 @@
-export function copyAsJSON(data: any) {
-  const jsonString = JSON.stringify(data, null, 2)
-  navigator.clipboard.writeText(jsonString)
+interface Postcode {
+ code: string;
+ area: string;
+ region: string;
 }
 
-export function copyAsCSV(data: any[]) {
-  const headers = Object.keys(data[0]).join(',')
-  const rows = data.map(obj => Object.values(obj).join(','))
-  const csvString = [headers, ...rows].join('\n')
-  navigator.clipboard.writeText(csvString)
+type JsonSerializable =
+ | Postcode[]
+ | Record<string, string | number | boolean | null>;
+
+export function copyAsJSON(data: JsonSerializable) {
+ const jsonString = JSON.stringify(data, null, 2);
+ navigator.clipboard.writeText(jsonString);
 }
 
+export function copyAsCSV(data: Postcode[]) {
+ const headers = Object.keys(data[0]).join(",");
+ const rows = data.map((obj) => Object.values(obj).join(","));
+ const csvString = [headers, ...rows].join("\n");
+ navigator.clipboard.writeText(csvString);
+}
